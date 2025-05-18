3d6787650e5b90bd25b4b76bc78ef64f087b88cf
ashif57 <ashifnavheed@gmail.com>
2025-05-18 12:19:28 +0530

    final
    none

--- Server/test_connection.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-from supabase import create_client
-
-# 1. Get the absolute path to .env
-env_path = Path(__file__).parent / '.env'
-load_dotenv(dotenv_path=env_path, override=True)
-
